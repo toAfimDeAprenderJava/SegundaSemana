ffd0ac3a62f39a8a50e0cddda8cd9975e729863d
brvnv <bruno.rdx@metalfan.com>
2018-10-16 10:34:58 -0300

    Commit inicial. Exercício feito, funcionando

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -5,17 +5,22 @@ package semana2;
  * 
  * Exemplo: 
  * 
- * Ás de Ouros
- * Ás de Espadas
- * Ás de Copas
- * Ás de Paus
+ * Ã�s de Ouros
+ * Ã�s de Espadas
+ * Ã�s de Copas
+ * Ã�s de Paus
  * Dois de Ouros
  * ...
  * 
  */
 public class Baralho {
 	public static void main(String[] args) {
-	
-		}
+	String[] numeros={"Ás", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez"};
+	String[] figuras={"ouros", "paus", "copas", "espadas"};
+	for(int numeroCorrente=0; numeroCorrente <=9; numeroCorrente++) {
+		for(int figuraCorrente=0; figuraCorrente <=3; figuraCorrente++) {
+			System.out.println(numeros[(numeroCorrente)] + " de " + figuras[(figuraCorrente)]);
 	}
+	}
+}
 }
