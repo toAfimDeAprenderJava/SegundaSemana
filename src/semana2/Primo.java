6b60f13656e41ad8c6a6beafb68c477a3567afd8
brvnv <bruno.rdx@metalfan.com>
2018-10-17 09:58:33 -0300

    Eu as vezes tenho umas ideias seriamente estúpidas. Ok, algo mudaria ao inverter a ordem da comparação... A primeira falharia e o interpretador nem perde tempo comparando a segunda. Tentei substituir o 1 para números que sei que são primos, para verificar se não era algum problema com meu while, não é. Hora de debugar mais uma vez

--- src/semana2/Primo.java
+++ src/semana2/Primo.java
@@ -8,7 +8,7 @@ package semana2;
 public class Primo {
 	public static void main(String[] args) {
 	for (int numeroPrimo=1; numeroPrimo<=100; numeroPrimo++) {
-		while((numeroPrimo%1==0) && (numeroPrimo%numeroPrimo!=0)) {
+		while((numeroPrimo%numeroPrimo!=0) && (numeroPrimo%1==0)) {
 			System.out.println(numeroPrimo);
 		}
 	}
