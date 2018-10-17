1ddce921dffb4f4e41a698626f2191260ee63247
Cesar Monti <cesarmonti17@gmail.com>
2018-10-17 15:08:44 -0300

    Exercicio numero primos concluido

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -15,7 +15,11 @@ package semana2;
  */
 public class Baralho {
 	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; ++i) {
+			System.out.println(i  +  " De Paus");
+		}
 	
 		}
 	}
-}
+
