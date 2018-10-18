1e382abb17cac98c77dcc0ce3b1a5cf5c0833f61
brvnv <bruno.rdx@metalfan.com>
2018-10-18 11:46:48 -0300

    Critérios mais precisos, e agora ele imprime 1, que nem primo é

--- src/semana2/Primo.java
+++ src/semana2/Primo.java
@@ -8,17 +8,18 @@ package semana2;
 public class Primo {
 
 public static void main(String[] args) {
-
 for(int numeroPrimo=1; numeroPrimo<=100; numeroPrimo++) {
 	if(ePrimo(numeroPrimo)) System.out.println(numeroPrimo);
 }
 }
 
 public static boolean ePrimo(int numeroPrimo) {
-	for(int checaPrimoAPrimo=2; checaPrimoAPrimo<=numeroPrimo; checaPrimoAPrimo++) {
-		if((numeroPrimo%checaPrimoAPrimo!=0) && (numeroPrimo/checaPrimoAPrimo<=numeroPrimo)) return true;
-		return false;
+	for(int checaNumANum=1; checaNumANum<=numeroPrimo; checaNumANum++) {
+		if((numeroPrimo%checaNumANum==0) && (checaNumANum!=numeroPrimo)) return false;
+		if((numeroPrimo%checaNumANum==0) && (checaNumANum!=numeroPrimo) && (checaNumANum!=1)) return false;
+		if((numeroPrimo%checaNumANum==0) && (checaNumANum!=numeroPrimo) && (numeroPrimo!=1)) return false;
+		return true;
 	}
-	return false; //Parece que tenho que acrescentar isso senão Java duvida que o for será executado e tem medo de não retornar nada
-	}
+	return false; //Linha redundante, mas o interpretador desconfia que o for não será executado e nada seria retornado	}
+}
 }
