bb4d64f72374e647b14fbcbeff5a2d2f362713ce
brvnv <bruno.rdx@metalfan.com>
2018-10-18 14:22:18 -0300

    Hey, faltou valete, rainha e rei. Corrigido

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -15,9 +15,9 @@ package semana2;
  */
 public class Baralho {
 	public static void main(String[] args) {
-	String[] numeros={"Ás", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez"};
+	String[] numeros={"Ás", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete", "Rainha", "Rei"};
 	String[] figuras={"ouros", "espadas", "copas", "paus"};
-	for(int numeroCorrente=0; numeroCorrente <=9; numeroCorrente++) {
+	for(int numeroCorrente=0; numeroCorrente <=12; numeroCorrente++) {
 		for(int figuraCorrente=0; figuraCorrente <=3; figuraCorrente++) {
 			System.out.println(numeros[(numeroCorrente)] + " de " + figuras[(figuraCorrente)]);
 	}
