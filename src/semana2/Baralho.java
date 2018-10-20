7f052a4b53da8b75664568757608a335107c50fc
biancalopes <bianca.santana.lopes.bs@gmail.com>
2018-10-20 17:23:32 -0300

    Adicionei comentario ao exercicio baralho

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -12,14 +12,16 @@ package semana2;
 public class Baralho {
 	public static void main(String[] args) {
 
+		//Declaracao das variaveis
 		String[] naipes = { "Ouros", "Paus", "Espadas", "Copas" };
 		String[] numeros = { "As", "Dois", "Tres", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete",
 				"Dama", "Rei" };
-
+		//iteracao vetores
 		int i, j;
 		
+		//For percorrendo vetor de Naipes
 		for (j = 0; j <= 3; j++) {
-
+		//For percorrendo vetor de numeros	
 		for (i = 0; i <= 12; i++) {
 
 				System.out.println(numeros[i] + " de " + naipes[j]);
