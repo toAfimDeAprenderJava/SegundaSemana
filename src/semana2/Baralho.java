a7776f050722dcf96dd9cb18bcf93af99b0a5a8c
biancalopes <bianca.santana.lopes.bs@gmail.com>
2018-10-20 17:18:37 -0300

    Exercicio baralho resolvido

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -3,19 +3,29 @@ package semana2;
 /**
  * Crie um programa que imprima todas as cartas do baralho.
  * 
- * Exemplo: 
+ * Exemplo:
  * 
- * Ás de Ouros
- * Ás de Espadas
- * Ás de Copas
- * Ás de Paus
- * Dois de Ouros
+ * 
  * ...
  * 
  */
 public class Baralho {
 	public static void main(String[] args) {
-	
+
+		String[] naipes = { "Ouros", "Paus", "Espadas", "Copas" };
+		String[] numeros = { "As", "Dois", "Tres", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete",
+				"Dama", "Rei" };
+
+		int i, j;
+		
+		for (j = 0; j <= 3; j++) {
+
+		for (i = 0; i <= 12; i++) {
+
+				System.out.println(numeros[i] + " de " + naipes[j]);
+			}
+
 		}
+
 	}
 }
