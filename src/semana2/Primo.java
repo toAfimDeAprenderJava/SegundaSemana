b5286d713778866e2745e7fc00f77f633bcaa893
siqcris <42442372+siqcris@users.noreply.github.com>
2018-10-21 15:40:17 -0300

    Update Primo.java
    Update

--- src/semana2/Primo.java
+++ src/semana2/Primo.java
@@ -5,8 +5,27 @@ package semana2;
  * primos de 1 à 100
  */
 public class Primo {
+
 	public static void main(String[] args) {
-		
-	
+		int i = 0;
+		int j = 0;
+		int intervalo = 101;
+
+		for (i = 1; i < intervalo; i++) {
+			boolean ehPrimo = true;
+			for (j = 1; j <= i; j++) {
+				if (((i % j) == 0) && (j != i)) {
+					ehPrimo = false;
+					break;
+				}
+			}
+
+			if (ehPrimo) {
+				System.out.println(i + " é um número primo");
+			} else {
+				System.out.println(i + " não é um número primo");
+			}
+		}
+
 	}
 }
