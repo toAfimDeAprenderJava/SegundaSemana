6eb5f2d5660ec7eab41da46301c3efb4ba128670
DeyseSouza <deyseluciasp@hotmail.com>
2018-10-21 10:38:00 -0300

    exercicio sgunda semana

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -5,17 +5,25 @@ package semana2;
  * 
  * Exemplo: 
  * 
- * Ás de Ouros
- * Ás de Espadas
- * Ás de Copas
- * Ás de Paus
+ * Ã�s de Ouros
+ * Ã�s de Espadas
+ * Ã�s de Copas
+ * Ã�s de Paus
  * Dois de Ouros
  * ...
  * 
  */
 public class Baralho {
 	public static void main(String[] args) {
+		String cartas = "As de " ;
+		String cartas1 = "Dois de " ;
+		String  naipesCartas = "Ouros Espadas Copas Paus Ouros" ;
+		String [] vect = naipesCartas.split(" ");
 	
-		}
+		System.out.println(cartas  + vect[0]);
+		System.out.println(cartas  + vect[1]);
+		System.out.println(cartas  + vect[2]);
+		System.out.println(cartas  + vect[3]);
+		System.out.println(cartas1  + vect[4]);
 	}
 }
