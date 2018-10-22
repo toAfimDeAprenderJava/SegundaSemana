860402310051be459d5df833bebe4b3993c03435
DeyseSouza <deyseluciasp@hotmail.com>
2018-10-22 09:06:40 -0300

    correção de codigo

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -15,7 +15,70 @@ package semana2;
  */
 public class Baralho {
 	public static void main(String[] args) {
-		String cartas = "As de " ;
+		
+		String naipes[] =  {"Ouros", "Espadas","Copas", "Paus"}; 
+
+
+		 
+		String cartas[] = {"Ás","Dois","Tres","Quatro","Cinco", "Seis", "Sete", "Oito", "Nove", "Valete", "Dama", "Rei"}; 
+		
+ 
+		for (int i = 0; i < cartas.length; i++) 
+ 
+		{ 
+
+			for  (int j = 0; j < naipes.length; j++){ 
+
+
+ 
+				System.out.println(cartas[i] + " de "+naipes[j]); 
+
+
+ 
+			} 
+
+
+ 
+		} 
+
+
+ 
+	} 
+
+
+ 
+} 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*String cartas = "As de " ;
 		String cartas1 = "Dois de " ;
 		String  naipesCartas = "Ouros Espadas Copas Paus Ouros" ;
 		String [] vect = naipesCartas.split(" ");
@@ -24,6 +87,6 @@ public class Baralho {
 		System.out.println(cartas  + vect[1]);
 		System.out.println(cartas  + vect[2]);
 		System.out.println(cartas  + vect[3]);
-		System.out.println(cartas1  + vect[4]);
+		System.out.println(cartas1 + vect[4]);
 	}
-}
+}*/
