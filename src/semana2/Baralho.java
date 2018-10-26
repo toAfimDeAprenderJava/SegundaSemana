0109fb0a0d700122365ded51b0a93fd1e908acbd
NIUORNELAS <noilsonornelas@hotmail.com>
2018-10-25 21:12:34 -0300

    lição 2 comcluida

--- src/semana2/Baralho.java
+++ src/semana2/Baralho.java
@@ -5,17 +5,25 @@ package semana2;
  * 
  * Exemplo: 
  * 
- * Ás de Ouros
- * Ás de Espadas
- * Ás de Copas
- * Ás de Paus
+ * Ã�s de Ouros
+ * Ã�s de Espadas
+ * Ã�s de Copas
+ * Ã�s de Paus
  * Dois de Ouros
  * ...
  * 
  */
 public class Baralho {
 	public static void main(String[] args) {
-	
-		}
+	// Cartas do Baralho
+		
+String vetor[] = {"As","2","3","4","5","6","7","8","9","10","J-Valéte","Q-Dama","K-Rei"};
+		String nipe [] = {"Copas","Ouro","Espadas","Paus"}; 
+int i=0;
+int c=0;
+for (i=0; i<vetor.length; i++)
+	for (c=0; c<nipe.length; c++)
+		System.out.println (vetor[i]+" de "+nipe[c]);
 	}
+	
 }
